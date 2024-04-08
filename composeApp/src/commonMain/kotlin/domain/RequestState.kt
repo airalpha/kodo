2fa6396172ab4825841dc96e07382b050242cb72
AIRALPHA <allphaair@gmail.com>
2024-04-08 23:58:30 +0200

    create model and requeststate

---
+++ composeApp/src/commonMain/kotlin/domain/RequestState.kt
@@ -0,0 +1,30 @@
+package domain
+
+sealed class RequestState<out T> {
+    data object Idle: RequestState<Nothing>()
+    data object Loading: RequestState<Nothing>()
+    data class Success<T>(val data: T): RequestState<T>()
+    data class Error(val message: String): RequestState<Nothing>()
+
+    fun isLoading() = this is Loading
+    fun isSuccess() = this is Success
+    fun isError() = this is Error
+
+    fun getSuccessData() = (this as Success).data
+    fun getSuccessDataOrNull(): T? {
+        return try {
+            (this as Success).data
+        } catch (e: Exception) {
+            null
+        }
+    }
+
+    fun getErrorMessage() = (this as Error).message
+    fun getErrorMessageOrEmpty(): String {
+        return try {
+            (this as Error).message
+        } catch (e: Exception) {
+            ""
+        }
+    }
+}
--- composeApp/src/commonMain/kotlin/domain/TodoTask.kt
+++ composeApp/src/commonMain/kotlin/domain/TodoTask.kt
@@ -1,4 +1,14 @@
 package domain
 
-class TodoTask {
+import io.realm.kotlin.types.RealmObject
+import io.realm.kotlin.types.annotations.PrimaryKey
+import org.mongodb.kbson.ObjectId
+
+class TodoTask: RealmObject {
+    @PrimaryKey
+    var _id: ObjectId = ObjectId()
+    var title: String = ""
+    var description: String = ""
+    var favorite: Boolean = false
+    var completed: Boolean = false
 }
