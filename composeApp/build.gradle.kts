d98ac472df112e06bad308797851c40aac6faf73
AIRALPHA <allphaair@gmail.com>
2024-04-08 17:41:35 +0200

    install realm db

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsCompose)
+    alias(libs.plugins.realmKotlin)
 }
 
 kotlin {
@@ -47,6 +48,10 @@ kotlin {
 
             implementation(project.dependencies.platform(libs.koin.bom))
             implementation(libs.koin.core)
+
+            implementation(libs.realm.library.base)
+            implementation(libs.kotlinx.coroutines)
+            implementation(libs.stately.common)
         }
     }
 }
